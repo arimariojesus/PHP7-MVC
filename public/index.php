c11203d57b1cf797a107c539b785ddaff17a0c03
Ari Jesus <arimario.jesus@hotmail.com>
2020-12-25 23:49:05 -0300

    initial commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+include("./../app/Libraries/Routes.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso de PHP7+ e MVC</title>
+</head>
+<body>
+  <?php
+    $routes = new Routes();
+  ?>
+
+</body>
+</html>
