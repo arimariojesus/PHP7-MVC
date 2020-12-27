884bd2ca1ed1f23f9593e168f072b043f661dbdc
Ari Jesus <arimario.jesus@hotmail.com>
2020-12-26 22:15:44 -0300

    update: added views header, footer and home

---
+++ app/Views/header.php
@@ -0,0 +1,27 @@
+<header class="bg-dark">
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">UnSet</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" data-tookit="tooltip" title="Página Inicial" href="<?=URL?>">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toolkit="tooltip" title="Sobre nós" href="<?=URL?>/pages/about">Sobre nós</a>
+            </li>
+          </ul>
+          <span class="navbar-text">
+            <a class="btn btn-info" href="#" data-tookit="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
+            <a class="btn btn-info" href="#" data-tookit="tooltip" title="Tem uma conta? Faça login">Entrar</a>
+          </span>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Configuration file
+*/
+
+// __FILE__ - Magic constant. Returns of complete path and name of file.
+
+//dirname - returns the path by removing the current directory/file
+define('APP', dirname(__FILE__));
+define('URL', 'http://localhost/framework');
+define('APP_NAME', 'Curso de PHP7 OOP e MVC');
+
+const APP_VERSION = '1.0.0';
+
+?>
