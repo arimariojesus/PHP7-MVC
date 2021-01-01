a6a63d91a7c4e52ac96a42a7f12b178dba492aa4
Ari Jesus <arimario.jesus@hotmail.com>
2021-01-01 15:44:50 -0300

    update: validation register

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -4,6 +4,14 @@ class Users extends Controller {
 
   public function register() {
 
+    $data = [
+      'name' => '',
+      'email' => '',
+      'password' => '',
+      'confirm_password' => '',
+      'name_error' => '',
+    ];
+
     $form = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
     if(isset($form)) {
       $data = [
@@ -12,13 +20,26 @@ class Users extends Controller {
         'password' => trim($form['password']),
         'confirm_password' => trim($form['confirm_password']),
       ];
-    }else {
-      $data = [
-        'name' => '',
-        'email' => '',
-        'password' => '',
-        'confirm_password' => '',
-      ];
+
+      if(empty($form['name'])) {
+        $data['name_error'] = 'Nome é obrigatório';
+      }
+
+      if(empty($form['email'])) {
+        $data['email_error'] = 'E-mail é obrigatório';
+      }
+
+      if(empty($form['password'])) {
+        $data['password_error'] = 'Informe a sua senha';
+      }else if(strlen($form['password']) < 6 || strlen($form['password']) > 20) {
+        $data['password_error'] = 'A senha deve ter entre 6 e 20 caracteres';
+      }
+
+      if(empty($form['confirm_password'])) {
+        $data['confirm_password_error'] = 'Confirme a sua senha';
+      }else if($form['password'] != $form['confirm_password']) {
+        $data['confirm_password_error'] = 'As senhas não coincidem';
+      }
     }
 
     $this->view('users/register', $data);
--- app/Views/users/register.php
+++ app/Views/users/register.php
@@ -9,19 +9,31 @@
       <form action="<?= URL ?>/users/register" name="register" method="POST" class="mt-4">
         <div class="form-group">
           <label for="name" class="form-label">Nome: <sup class="text-danger">*</sup></label>
-          <input type="text" name="name" id="name" class="form-control" value="<?=$data['name']?>" required>
+          <input type="text" name="name" id="name" value="<?=$data['name']?>" class="form-control <?= isset($data['name_error']) ? 'is-invalid' : '' ?>">
+          <div class="invalid-feedback">
+            <?= $data['name_error'] ?>
+          </div>
         </div>
         <div class="form-group">
           <label for="email" class="form-label">E-mail: <sup class="text-danger">*</sup></label>
-          <input type="email" name="email" id="email" class="form-control" value="<?=$data['email']?>" required>
+          <input type="email" name="email" id="email" value="<?=$data['email']?>"class="form-control <?= isset($data['email_error']) ? 'is-invalid' : '' ?>">
+          <div class="invalid-feedback">
+            <?= $data['email_error'] ?>
+          </div>
         </div>
         <div class="form-group">
           <label for="password" class="form-label">Senha: <sup class="text-danger">*</sup></label>
-          <input type="password" name="password" id="password" class="form-control" value="<?=$data['password']?>" required>
+          <input type="password" name="password" id="password" value="<?=$data['password']?>"class="form-control <?= isset($data['password_error']) ? 'is-invalid' : '' ?>">
+          <div class="invalid-feedback">
+            <?= $data['password_error'] ?>
+          </div>
         </div>
         <div class="form-group">
           <label for="confirm_password" class="form-label">Confirme a Senha: <sup class="text-danger">*</sup></label>
-          <input type="password" name="confirm_password" id="confirm_password" class="form-control" value="<?= $data['confirm_password']?>" required>
+          <input type="password" name="confirm_password" id="confirm_password" value="<?= $data['confirm_password']?>" class="form-control <?= isset($data['confirm_password_error']) ? 'is-invalid' : '' ?>">
+          <div class="invalid-feedback">
+            <?= $data['confirm_password_error'] ?>
+          </div>
         </div>
 
         <div class="row mt-3">
