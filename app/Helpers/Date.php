e42cff2d1a46792f482cc7ffb524527827d8dd8f
Ari Jesus <arimario.jesus@hotmail.com>
2021-01-12 15:03:53 -0300

    feat(Date): convert language for portuguese-BR

--- app/Helpers/Date.php
+++ app/Helpers/Date.php
@@ -4,10 +4,11 @@ class Date {
   
   public static function formatDate($date) {
     $day = date('d', strtotime($date));
-    $month = date('F', strtotime($date));
+    $month = date('m', strtotime($date));
     $year = date('Y', strtotime($date));
 
-    return $day." de ".$month." de ".$year;
+    setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+    return strftime("%e de %B de %Y", mktime($day, $month, $year));
   }
 
 }
